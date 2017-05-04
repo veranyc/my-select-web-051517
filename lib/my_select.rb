f9a94d4de090782e6e7c3e756fb6f49372af1e9a
Vera Zago <veranyc@gmail.com>
2017-05-04 13:11:14 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+ i = 0 #counter
+ selected_from_collection = [] #new array
+ while i < collection.length
+#take a given block which has code parameters. Add to the new array whatever value is true.
+  if (yield(collection[i])) #if that block of code is true
+    selected_from_collection << collection[i] #then add that value to array
+  end
+   i += 1 #increase the counter by 1 and go back to the start of the while loop
+ end # close the while loop
+ selected_from_collection #return the new array
 end
